130a9ae06b9fed6b50d63c08c3c1cd34c3a7ee9f
kaka <iamkimseunghyun@gmail.com>
2022-05-15 16:18:05 +0900

    Implement #3 - SortService
    정렬 구현체를 사용하는 서비스를 별도로 작성
    의존성 주입 방법을 적용

---
+++ src/main/java/io/laaf/oop/service/SortService.java
@@ -0,0 +1,20 @@
+package io.laaf.oop.service;
+
+import io.laaf.oop.logic.JavaSort;
+import io.laaf.oop.logic.Sort;
+
+import java.util.List;
+
+public class SortService {
+
+    private final Sort<String> sort;
+
+    public SortService(Sort<String> sort) {
+        this.sort = sort;
+        System.out.println("구현체: " + sort.getClass().getName());
+    }
+
+    public List<String> doSort(List<String> list) {
+        return sort.sort(list);
+    }
+}
